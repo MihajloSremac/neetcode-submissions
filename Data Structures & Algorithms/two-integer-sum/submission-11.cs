18e42725d2fa0e98696fe1f0c472ac996366982b
Mihajlo Sremac <92401223+MihajloSremac@users.noreply.github.com>
2026-08-04 22:31:13 +0200

    Add: two-integer-sum - submission-11

---
+++ Data Structures & Algorithms/two-integer-sum/submission-11.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        Dictionary<int, int> hashNums = new Dictionary<int, int>();
+
+        for(int i = 0; i < nums.Length; i++){
+            if(hashNums.ContainsKey(target-nums[i]))
+                return [hashNums[target-nums[i]], i];
+            hashNums.Add(nums[i], i);
+        }
+
+        return [0,0];
+    }
+}
